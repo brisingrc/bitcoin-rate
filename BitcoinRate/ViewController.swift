586ac150c06e78d9c50eb0b08b7c94ffe2567d50
tima <tima.yegemberdiev@gmail.com>
2019-06-14 17:31:28 +0600

    Initial Commit

---
+++ BitcoinRate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitcoinRate
+//
+//  Created by Timur on 6/14/19.
+//  Copyright © 2019 BitcoinRate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
